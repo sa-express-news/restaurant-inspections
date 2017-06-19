702c4b8112157b6e8153d576781768a7443a6d6f
Kia Farhang <kfarhang0@gmail.com>
2017-06-19 15:12:55 -0500

    Added

---
+++ src/pageFetcher.js
@@ -0,0 +1,17 @@
+// @flow
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+export function fetchPage(url: string) {
+    return new Promise((resolve, reject) => {
+        if (Object.prototype.toString.call(url) !== "[object String]") {
+            return reject('Variable passed to fetchPage is not a string');
+        } else {
+            JSDOM.fromURL('http://example.org').then(dom => {
+                const page = dom.window.document;
+                return resolve(page);
+            });
+        }
+    });
+}
