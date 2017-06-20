40591835bbae5ef1f3c8fcba5f72100d7f80f60c
Kia Farhang <kfarhang0@gmail.com>
2017-06-20 11:57:05 -0500

    Near

--- src/inspectionLinkCollector.js
+++ src/inspectionLinkCollector.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { fetchPage } from './pageFetcher';
+import { isInspectionPageLink } from './inspectionPageCollector';
 
 export function collectInspectionLinks(pages: Array < string > ) {
     return new Promise((resolve, reject) => {
@@ -8,38 +9,37 @@ export function collectInspectionLinks(pages: Array < string > ) {
             return reject('Variable passed to collectInspectionLinks is not an array');
         } else if (!pages.every(isString)) {
             return reject('Array passed to collectInspectionLinks must contain only strings');
+        } else if (!pages.every(isInspectionPageLink)) {
+            return reject('Strings/links passed to collectInspectionLinks are not search links');
+        } else {
+            return resolve(fetchPage('http://tx.healthinspections.us/san%20antonio/search.cfm?start=1&1=1&sd=05/21/2017&ed=06/20/2017&kw1=&kw2=&kw3=&rel1=L.licenseName&rel2=L.licenseName&rel3=L.licenseName&zc=&dtRng=YES&pre=similar&smoking=ANY')
+                .then((page) => {
+                    const links = Object.values(page.getElementsByTagName('a')).map((object) => {
+                        return object.href;
+                    });
+                    const inspectionLinks = deduplicateArray(links.filter(isInspectionLink));
+                    return inspectionLinks;
+                }));
         }
-
-        // const test = pages.every(isString);
-        // if (!test) {
-        //    
-        // } else {
-        //     return resolve('hi');
-        // }
-        return resolve('hi');
-
-        function isString(item) {
-            return Object.prototype.toString.call(item) === "[object String]";
-        }
-        //     if (Object.prototype.toString.call(url) !== "[object String]") {
-        //         return reject('Variable passed to collectPages is not a string');
-        //     } else {
-        //         return resolve(fetchPage(url)
-        //             .then((page) => {
-        //                 const links = Object.values(page.getElementsByTagName('a'));
-        //                 const pageLinks = links.filter(filterLinks).map((link)=>{
-        //                     return link.href;
-        //                 });
-        //                 return pageLinks;
-        //             }));
-
-        //         function filterLinks(link){
-        //             return link.href.indexOf('search.cfm?start=') !== -1;
-        //         }
-        //     }
-
-
     });
+}
 
+export function isString(item: string) {
+    return Object.prototype.toString.call(item) === "[object String]";
+}
+
+export function isInspectionLink(link: string) {
+    return link.indexOf('http://tx.healthinspections.us/san%20antonio/estab.cfm?') !== -1;
+}
+
+export function deduplicateArray(array: Array < any > ) {
+    if (!Array.isArray(array)) {
+        throw Error('variable passed to deduplicateArray is not an array');
+    } else {
+        let newArray = [];
+        const set = new Set(array);
+        for (let item of set) newArray.push(item);
+        return newArray;
+    }
 
 }
